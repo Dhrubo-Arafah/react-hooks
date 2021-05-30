8f161ca892d9ba8f2d709608e654da5ed9310f82
Arafah Dhrubo <samiul15-13051@diu.edu.bd>
2021-05-30 06:44:16 +0600

    Run useEffect() only once

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import ClassCounterOne from './components/useEffect/ClassCounterOne';
-import HookCounterOne from './components/useEffect/HookCounterOne';
+import ClassMouse from './components/useEffect/LectureTwo/ClassMouse';
+import HookMouse from './components/useEffect/LectureTwo/HookMouse';
 
 function App() {
   return (
     <div className="App">
-      <HookCounterOne />
-      <ClassCounterOne/>
+      <ClassMouse />
+      <HookMouse/>
     </div>
   );
 }
